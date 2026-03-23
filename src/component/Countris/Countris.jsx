50c3992956df1bce4aa82b5d24b8589d8c0ba639
sabilulbari <mdsabilulbari100@gmail.com>
2026-03-23 16:49:06 +0600

    Added selection event

--- src/component/Countris/Countris.jsx
+++ src/component/Countris/Countris.jsx
@@ -1,15 +1,19 @@
-import React, { use } from 'react';
-import Country from '../country/Country';
-import '../../App.css'
+import React, { use } from "react";
+import Country from "../country/Country";
+import "../../App.css";
 
 const Countris = ({ allCountris }) => {
   const countris = use(allCountris);
   const countries = countris.countries;
-  
+
   return (
     <>
-      <div  className='main-body'>
+      <div className="main-body">
         <h1>Total Country: {countris.countries.length}</h1>
+        <div className="visit-count">
+          <h2>Visited country: </h2>
+          <h2>Not visited coountr: </h2>
+        </div>
         <div className="card">
           {countries.map((country) => (
             <Country key={country.ccn3.ccn3} country={country}></Country>
--- src/component/country/Country.jsx
+++ src/component/country/Country.jsx
@@ -1,15 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Country = ({ country }) => {
-//   console.log(country.ccn3.ccn3);
+
+  const [setvisit, stateSetvisit] = useState(false);
+
+    
+
+    const handleClick = ()=>{
+      // if(setvisit ==true){
+      //   stateSetvisit(false);
+      // }else{
+      //   stateSetvisit(false);
+      // }
+      // stateSetvisit(setvisit ? false : true);
+      stateSetvisit(!setvisit);
+    }
+
+
+//   console.log(country.area.area);
   return (
-    <div className="cardComponent">
+    <div className={`cardComponent ${setvisit ? "visited-card" : ""}`}>
       <img src={country.flags.flags.svg} alt={country.flags.flags.png} />
       <div>
         <h3>{country.name.common}</h3>
 
         <p>Population: {country.population.population}</p>
         <p>Country Code: {country.ccn3.ccn3}</p>
+        <p>
+          Area: {country.area.area} {country.area.area > 300000 ? <h3>"Big Country"</h3> : <h3>"Small Country"</h3>}
+        </p>
+        <button className={`${setvisit ? "visited-button" : ""}`} onClick={handleClick}>
+          {setvisit ? "Visited" : "Not Visited"}
+        </button>
       </div>
     </div>
   );
